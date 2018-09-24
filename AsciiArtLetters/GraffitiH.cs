44bb267bf4c05369f63e04c77dd153d698166a55
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2018-09-24 18:49:25 +0300

    Add files via upload

---
+++ AsciiArtLetters/GraffitiH.cs
@@ -0,0 +1,580 @@
+﻿using System;
+namespace AsciiArtLetters
+{
+    public static class GraffitiH
+    {
+		public static string a()
+		{
+            // return this string at the end
+			string letterstr = String.Empty;
+
+			// make a readable array of the ascii art letter
+			string[] letterarray =
+			{
+				@"   _____   ",
+				@"  /  _  \  ",
+				@" /  /_\  \ ",
+				@"/    |    \",
+				@"\____|__  /",
+				@"        \/ "
+			};
+
+            // turn the array to a string using a for loop
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			// return the final string
+			return letterstr;
+		}
+
+
+		public static string b()
+		{
+			// the same thing with lettera()
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"__________ ",
+				@"\______   \",
+				@" |    |  _/",
+				@" |    |   \",
+				@" |______  /",
+				@"        \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n"; 
+			}
+
+			return letterstr;
+		}
+
+		public static string c()
+		{
+			// same thing with lettera() and letterb()
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"_________  ",
+				@"\_   ___ \ ",
+				@"/    \  \/ ",
+				@"\     \____",
+				@" \______  /",
+				@"        \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string d()
+		{
+			// you get the point
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"________   ",
+				@"\______ \  ",
+				@" |    |  \ ",
+				@" |    `   \",
+				@"/_______  /",
+				@"        \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string e()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"___________",
+				@"\_   _____/",
+				@" |    __)_ ",
+				@" |        \",
+				@"/_______  /",
+				@"        \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string f()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"___________",
+				@"\_   _____/",
+				@" |    __)  ",
+				@" |     \   ",
+				@" \___  /   ",
+				@"     \/    "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string g()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"  ________ ",
+				@" /  _____/ ",
+				@"/   \  ___ ",
+				@"\    \_\  \",
+				@" \______  /",
+				@"        \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+		public static string h()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"  ___ ___  ",
+				@" /   |   \ ",
+				@"/    ~    \",
+				@"\    Y    /",
+				@" \___|_  / ",
+				@"       \/  "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+                
+			return letterstr;
+		}
+
+		public static string i()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@".___ ",
+				@"|   |",
+				@"|   |",
+				@"|   |",
+				@"|___|"
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			};
+
+			return letterstr;
+		}
+
+		public static string j()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"     ____.",
+				@"    |    |",
+				@"    |    |",
+				@"/\__|    |",
+				@"\________|"
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+		public static string k()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@" ____  __.",
+				@"|    |/ _|",
+				@"|      <  ",
+				@"|    |  \ ",
+				@"|____|__ \",
+				@"        \/"
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string l()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@".____     ",
+				@"|    |    ",
+				@"|    |    ",
+				@"|    |___ ",
+				@"|_______ \",
+				@"        \/"
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+		public static string m()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"   _____   ",
+				@"  /     \  ",
+				@" /  \ /  \ ",
+				@"/    Y    \",
+				@"\____|__  /",
+				@"        \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr = letterarray[i] + "\n";	
+			};
+
+			return letterstr;
+		}
+
+        public static string n() 
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@" _______   ",
+				@" \      \  ",
+				@" /   |   \ ",
+				@"/    |    \",
+				@"\____|__  /",
+				@"        \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string o() 
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"________   ",
+				@"\_____  \  ",
+				@" /   |   \ ",
+				@"/    |    \",
+				@"\_______  /",
+				@"        \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+		public static string p()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"__________ ",
+				@"\______   \",
+				@" |     ___/",
+				@" |    |    ",
+				@" |____|    "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string q()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"________   ",
+				@"\_____  \  ",
+				@" /  / \  \ ",
+				@"/   \_/.  \",
+				@"\_____\ \_/",
+                @"       \__>"
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string r() 
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"__________ ",
+				@"\______   \",
+				@" |       _/",
+				@" |    |   \",
+				@" |____|_  /",
+				@"        \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+		public static string s() 
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"  _________",
+				@" /   _____/",
+				@" \_____  \ ",
+				@" /        \",
+				@"/_______  /",
+				@"        \/ "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+		public static string t() 
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"___________",
+				@"\__    ___/",
+				@"  |    |   ",
+				@"  |    |   ",
+				@"  |____|   "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+        public static string u()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@" ____ ___ ",
+				@"|    |   \",
+				@"|    |   /",
+				@"|    |  / ",
+				@"|______/  "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+        public static string v()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"____   ____",
+				@"\   \ /   /",
+				@" \   Y   / ",
+				@"  \     /  ",
+				@"   \___/   "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+        public static string w()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"__  _  __",
+				@"\ \/ \/ /",
+				@" \     / ",
+				@"  \/\_/  "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+        
+        public static string x()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"____  ___",
+				@"\   \/  /",
+				@" \     / ",
+				@" /     \ ",
+				@"/___/\  \",
+				@"      \_/"    
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+		public static string y()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"_____.___.",
+				@"\__  |   |",
+				@" /   |   |",
+				@" \____   |",
+				@" / ______|",
+				@" \/       "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+        public static string z()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"__________",
+				@"\____    /",
+				@"  /     / ",
+				@" /     /_ ",
+				@"/_______ \",
+				@"        \/"
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+
+    }
+}
---
+++ AsciiArtLetters/GraffitiL.cs
@@ -0,0 +1,561 @@
+﻿using System;
+namespace AsciiArtLetters
+{
+    public static class GraffitiL
+    {
+		public static string a()
+		{
+            // return this string at the end
+			string letterstr = String.Empty;
+
+			// make a readable array of the ascii art letter
+			string[] letterarray =
+			{
+				@"_____   ",
+				@"\__  \  ",
+				@" / __ \_",
+				@"(____  /",
+				@"     \/ "
+			};
+
+            // turn the array to a string using a for loop
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			// return the final string
+			return letterstr;
+		}
+
+
+		public static string b()
+		{
+			// the same thing with lettera()
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"___.    ",
+				@"\_ |__  ",
+				@" | __ \ ",
+				@" | \_\ \",
+				@" |___  /",
+				@"     \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n"; 
+			}
+
+			return letterstr;
+		}
+
+		public static string c()
+		{
+			// same thing with lettera() and letterb()
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"  ____  ",
+				@"_/ ___\ ",
+				@"\  \___ ",
+				@" \___  >",
+				@"     \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string d()
+		{
+			// you get the point
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"    .___",
+				@"  __| _/",
+				@" / __ | ",
+				@"/ /_/ | ",
+				@"\____ | ",
+				@"     \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string e()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"  ____  ",
+				@"_/ __ \ ",
+				@"\  ___/ ",
+				@" \___  >",
+				@"     \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string f()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"  _____ ",
+				@"_/ ____\",
+				@"\   __\ ",
+				@" |  |   ",
+				@" |__|   "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string g()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"   ____  ",
+				@"  / ___\ ",
+				@" / /_/  >",
+				@" \___  / ",
+				@" \___  / "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+		public static string h()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@".__     ",
+				@"|  |__  ",
+				@"|  |  \ ",
+				@"|   Y  \",
+				@"|___|  /",
+				@"     \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+                
+			return letterstr;
+		}
+
+		public static string i()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@".__ ",
+				@"|__|",
+				@"|  |",
+				@"|  |",
+				@"|__|"
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			};
+
+			return letterstr;
+		}
+
+		public static string j()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"     __ ",
+				@"    |__|",
+				@"    |  |",
+				@"    |  |",
+				@"/\__|  |",
+				@"\______|"
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+		public static string k()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@" __    ",
+				@"|  | __",
+				@"|  |/ /",
+				@"|    / ",
+				@"|__|_ \",
+				@"     \/"
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string l()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@".__   ",
+				@"|  |  ",
+				@"|  |  ",
+				@"|  |__",
+				@"|____/"
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+		public static string m()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"  _____  ",
+				@" /     \ ",
+				@"|  Y Y  \",
+				@"|__|_|  /",
+				@"      \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr = letterarray[i] + "\n";	
+			};
+
+			return letterstr;
+		}
+
+        public static string n() 
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"  ____  ",
+				@" /    \ ",
+				@"|   |  \",
+				@"|___|  /",
+				@"     \/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string o() 
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"  ____  ",
+				@" /  _ \ ",
+				@"(  <_> )",
+				@" \____/ "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+		public static string p()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"______  ",
+				@"\____ \ ",
+				@"|  |_> >",
+				@"|   __/ ",
+				@"|__|    "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string q()
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"  ______",
+				@" / ____/",
+				@"< <_|  |",
+				@" \__   |",
+				@"    |__|"
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+        public static string r() 
+		{
+			string letterstr = String.Empty;
+
+			string[] letterarray =
+			{
+				@"_______ ",
+				@"\_  __ \",
+				@" |  | \/",
+				@" |__|   "
+			};
+
+			for (int i = 0; i < letterarray.Length; i++)
+			{
+				letterstr += letterarray[i] + "\n";
+			}
+
+			return letterstr;
+		}
+
+		public static string s() 
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"  ______",
+				@" /  ___/",
+				@" \___ \ ",
+				@"/____  >",
+				@"     \/ "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+		public static string t() 
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"  __   ",
+				@"_/  |_ ",
+				@"\   __\",
+				@" |  |  ",
+				@" |__|  "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+        public static string u()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@" __ __ ",
+				@"|  |  \",
+				@"|  |  /",
+				@"|____/ "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+        public static string v()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"___  __",
+				@"\  \/ /",
+				@" \   / ",
+				@"  \_/  "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+        public static string w()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"__  _  __",
+				@"\ \/ \/ /",
+				@" \     / ",
+				@"  \/\_/  "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+        
+        public static string x()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"___  ___",
+				@"\  \/  /",
+				@" >    < ",
+				@"/__/\__\"
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+		public static string y()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@" ___.__.",
+				@"<   |  |",
+				@" \___  |",
+				@" / ____|",
+				@" \/     "
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+        public static string z()
+		{
+			string letterstr = String.Empty;
+
+            string[] letterarray =
+            {
+				@"________",
+				@"\___   /",
+				@" /    / ",
+				@"/_____ \",
+				@"      \/"
+            };
+
+            for (int i = 0; i < letterarray.Length; i++)
+            {
+                letterstr += letterarray[i] + "\n";
+            }
+
+            return letterstr;
+		}
+
+
+    }
+}
